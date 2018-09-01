8c0b04287c824c17208c77990f28ee9b4eca275a
chenji <chenji000000@163.com>
2018-09-01 23:09:45 +0800

    MadLibs

--- MadLibs/MadLibs/ViewController.swift
+++ MadLibs/MadLibs/ViewController.swift
@@ -22,6 +22,22 @@ class ViewController: NSViewController {
     
     @IBOutlet weak var amountSilder: NSSlider!
     
+    @IBOutlet weak var datePicker: NSDatePicker!
+    
+    @IBOutlet weak var rwDevConRadioButton: NSButton!
+    
+    @IBOutlet weak var threeSixtyRadioButton: NSButton!
+    
+    @IBOutlet weak var wwdcRadioButton: NSButton!
+    
+    @IBOutlet weak var yellCheck: NSButton!
+    
+    @IBOutlet weak var voiceSegmentedControl: NSSegmentedControl!
+    
+    @IBOutlet weak var resultTextField: NSTextField!
+    
+    @IBOutlet weak var imageView: NSImageView!
+    
     fileprivate let singularNouns = ["dog", "muppet", "ninja", "pirate", "dev" ]
     fileprivate let pluralNouns = ["tacos", "rainbows", "iPhones", "gold coins"]
     
@@ -45,6 +61,20 @@ class ViewController: NSViewController {
     //2
     fileprivate let synth = NSSpeechSynthesizer()
     
+    fileprivate var selectedPlace: String {
+        var place = "home"
+        if rwDevConRadioButton.state == NSControl.StateValue.on {
+            place = "RWDevCon"
+        }
+        else if threeSixtyRadioButton.state == NSControl.StateValue.on {
+            place = "360iDev"
+        }
+        else if wwdcRadioButton.state == NSControl.StateValue.on {
+            place = "WWDC"
+        }
+        return place
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -64,6 +94,14 @@ class ViewController: NSViewController {
         phraseTextView.string = "Me coding Mac Apps!!!"
         // Update the amount slider
         sliderChanged(self)
+        // Set the date picker to display the current date
+        datePicker.dateValue = Date()
+        // Set the radio group's initial selection
+        rwDevConRadioButton.state = NSControl.StateValue.on
+        
+        yellCheck.state = NSControl.StateValue.off
+        // Set the segmented control initial selection
+        voiceSegmentedControl.selectedSegment = 1
         
     }
     
@@ -78,16 +116,49 @@ class ViewController: NSViewController {
     }
     
     @IBAction func goButtonClicked(_ sender: Any) {
+        // 1
         let pastTenseVerb = pastTenseVerbTextField.stringValue
+        
+        // 2
         let singularNoun = singularNounCombo.stringValue
+        
+        // 3
+        let amount = amountSilder.integerValue
+        
+        // 4
         let pluralNoun = pluralNouns[pluralNounPopup.indexOfSelectedItem]
+        
+        // 5
         let phrase = phraseTextView.string 
         
-        let madLibSentence = "A \(singularNoun) \(pastTenseVerb) \(pluralNoun) and said, \(phrase)!"
+        // 6
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        let date = dateFormatter.string(from: datePicker.dateValue)
         
-        print("\(madLibSentence)")
-        readSentence(madLibSentence, rate: .normal)
+        // 7
+        var voice = "said"
+        if yellCheck.state == NSControl.StateValue.on {
+            voice = "yelled"
+        }
+        
+        // 8
+        let sentence = "On \(date), at \(selectedPlace) a \(singularNoun) \(pastTenseVerb) \(amount) \(pluralNoun) and \(voice), \(phrase)"
+        
+        // 9
+        resultTextField.stringValue = sentence
+        imageView.image = NSImage(named: NSImage.Name(rawValue: "face"))
+        
+        // 10
+        let selectedSegment = voiceSegmentedControl.selectedSegment
+        let voiceRate = VoiceRate(rawValue: selectedSegment) ?? .normal
+        readSentence(sentence, rate: voiceRate)
     }
+    
+    @IBAction func radioButtonChanged(_ sender: AnyObject) {
+        
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
